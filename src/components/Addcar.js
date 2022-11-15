65ee533821f3453a5d7117fd047f27fab0305a52
sambba1 <samuli-lehtinen@hotmail.fi>
2022-11-15 05:13:07 +0200

    first commit

---
+++ src/components/Addcar.js
@@ -0,0 +1,114 @@
+import React, {useState} from 'react';
+import Button from'@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { PropertyKeys } from 'ag-grid-community';
+
+function Addcar(props){
+
+    const [open, setOpen] = useState(false);
+    const [car, setCar] = useState({
+        brand:'',
+        model:'',
+        color:'',
+        fuel:'',
+        year:'',
+        price:''
+    });
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleInputChange = (event) => {
+        setCar({...car, [event.target.name]: event.target.value});
+    };
+
+    const addCar = () => {
+        props.saveCar(car);
+        handleClose();
+    }
+
+
+    return(
+        <div>
+            <Button variant="outlined" color="primary" onClick={handleOpen}>Add car</Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">New Car</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="brand"
+                        value={car.brand}
+                        onChange={e => handleInputChange(e)}
+                        label="Brand"
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="model"
+                        value={car.model}
+                        onChange={e => handleInputChange(e)}
+                        label="Model"
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="color"
+                        value={car.color}
+                        onChange={e => handleInputChange(e)}
+                        label="Color"
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="fuel"
+                        value={car.fuel}
+                        onChange={e => handleInputChange(e)}
+                        label="Fuel"
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="year"
+                        value={car.year}
+                        onChange={e => handleInputChange(e)}
+                        label="Year"
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="price"
+                        value={car.price}
+                        onChange={e => handleInputChange(e)}
+                        label="Price"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={addCar} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+export default Addcar;
---
+++ src/components/Carlist.js
@@ -0,0 +1,70 @@
+import React, {useEffect, useState, useRef} from 'react';
+import { AgGridReact} from'ag-grid-react';
+import'ag-grid-community/dist/styles/ag-grid.css';
+import'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from'@mui/material/Button';
+import Addcar from './Addcar';
+
+function Carlist(){
+    const gridRef = useRef();
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => fetchData(), []);
+
+    const fetchData = () => {
+        fetch('https://carstockrest.herokuapp.com/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+        
+        
+    };
+
+    const deleteCar = (link) => {
+        if (window.confirm("Are you sure?")){
+            fetch(link, {method: 'DELETE'})
+            .then(res => fetchData())
+            .catch(err => console.log(err))
+        }
+    };
+
+    const saveCar = (car) =>{
+        fetch('https://carstockrest.herokuapp.com/cars', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(car)
+        })
+        .then(res => fetchData())
+        .catch(err => console.log(err))
+    };
+
+    const columns = [
+        {field: "brand"},
+        {field: "model"},
+        {field: "color"},
+        {field: "fuel"},
+        {field: "year"},
+        {field: "price"},
+        {
+            headerName: "",
+            field: "_links.self.href",
+            cellRenderer: function(field){
+                return <Button onClick={() => deleteCar(field.value)} >delete</Button>
+            },
+
+        }
+        
+    ]
+
+    return (
+        <div>
+            <div className="ag-theme-material"
+                style={{height: '900px', width: '100%', margin: 'center'}} >
+                    <Addcar saveCar={saveCar}/>
+                <AgGridReact rowData={cars} columnDefs={columns}></AgGridReact>
+            </div>
+        </div>
+    );
+};
+export default Carlist;
